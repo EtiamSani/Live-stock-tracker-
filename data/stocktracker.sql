40e9e849e62786de38da2f8829c20905bf7dd63e
EtiamSani <etiam.sani@laposte.net>
2023-02-12 15:49:32 +0100

    populate db

--- data/stocktracker.sql
+++ data/stocktracker.sql
@@ -1,18 +1,19 @@
+--DROP
 DROP TABLE IF EXISTS "watch_list_company", "company", "user_watchlist", "user", "watch_list"; 
 
+--CREATE TABLES
 CREATE TABLE "company" 
 (
-    "code_company" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "code_company" SERIAL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
     "symbol" VARCHAR(10) NOT NULL,
-    "entry_price" DECIMAL(10, 2) NULL, 
-    "logo" BLOB NULL
-
+    "entry_price" NUMERIC(10, 2) NULL, 
+    "logo" BYTEA NULL
 );
 
 CREATE TABLE "user" 
 (
-    "code_user" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "code_user" SERIAL PRIMARY KEY,
     "nickname" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NOT NULL UNIQUE,
     "password" VARCHAR(255) NOT NULL
@@ -21,8 +22,8 @@ CREATE TABLE "user"
 
 CREATE TABLE "watch_list" 
 (
-    "code_list" INTEGER GENERATED always AS IDENTITY PRIMARY KEY,
-    "name" VARCHAR(255) NOT NULL,   
+    "code_list" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL   
 );
 
 CREATE TABLE "watch_list_company"
@@ -30,8 +31,8 @@ CREATE TABLE "watch_list_company"
     "company_id" INTEGER NOT NULL,
     "list_id" INTEGER NOT NULL,
     PRIMARY KEY ("company_id", "list_id"),
-    CONSTRAINT "fk_watchlistcompany_company" FOREIGN KEY ("company_id") REFERENCES "company"("code_company") ON DELETE CASCADE,
-    CONSTRAINT "fk_watchlistcompany_list" FOREIGN KEY ("list_id") REFERENCES "watch_list"("code_list") ON DELETE CASCADE
+    FOREIGN KEY ("company_id") REFERENCES "company"("code_company") ON DELETE CASCADE,
+    FOREIGN KEY ("list_id") REFERENCES "watch_list"("code_list") ON DELETE CASCADE
 );
 
 CREATE TABLE "user_watchlist" 
@@ -39,9 +40,18 @@ CREATE TABLE "user_watchlist"
     "list_id" INTEGER NOT NULL,
     "user_id" INTEGER NOT NULL,
     PRIMARY KEY ("list_id", "user_id"),
-    CONSTRAINT "fk_userwatchlist_list" FOREIGN KEY ("list_id") REFERENCES "watch_list"("code_list") ON DELETE CASCADE,
-    CONSTRAINT "fk_userwatchlist_user" FOREIGN KEY ("user_id") REFERENCES "user"("code_user") ON DELETE CASCADE
+    FOREIGN KEY ("list_id") REFERENCES "watch_list"("code_list") ON DELETE CASCADE,
+    FOREIGN KEY ("user_id") REFERENCES "user"("code_user") ON DELETE CASCADE
 );
 
---seeding 
+--SEEDING 
 
+INSERT INTO "company" ("name","symbol","entry_price","logo" ) VALUES ('Align Technology', 'ALGN', 41.78, NULL);
+
+INSERT INTO "user" ("nickname","email","password" ) VALUES ('JsonStatham', 'json@json.com', '123');
+
+INSERT INTO "watch_list" ("name") VALUES ('Quality stocks');
+
+INSERT INTO "watch_list_company" ("company_id","list_id") VALUES (1,1);
+
+INSERT INTO "user_watchlist" ("list_id","user_id") VALUES (1,1);
