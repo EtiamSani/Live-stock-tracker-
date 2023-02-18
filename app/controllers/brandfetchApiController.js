76f3d9a5177c4525064eac2940337619dfbccf96
EtiamSani <etiam.sani@laposte.net>
2023-02-18 20:14:27 +0100

    customing API

--- app/controllers/brandfetchApiController.js
+++ app/controllers/brandfetchApiController.js
@@ -8,8 +8,20 @@ const brandFetchApiController = {
         const companyName = req.params.companyname +'.com'
         console.log(companyName)
         try {
+            let logoSrc = 0
             const reponse = await axios.get(`${URL_BRANDFETCH}${companyName}`,{ headers })
-            res.json(reponse.data)
+            const reponseLogo = reponse.data["logos"]
+            for (const logo of reponseLogo) {
+                
+                for (const format of logo.formats) {
+                    console.log(format["src"])
+                    logoSrc = format["src"] 
+                    break;    
+                } if (logoSrc) {
+                    break
+                }
+            }
+            res.json(logoSrc)
 
         } catch (error) {
             console.error(error);
