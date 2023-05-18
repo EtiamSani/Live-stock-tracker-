348561d1a982090103a17974b7cfce23bf93f936
EtiamSani <etiam.sani@laposte.net>
2023-05-18 17:02:41 +0200

    cleaning

--- app/controllers/finnhubApiController.js
+++ app/controllers/finnhubApiController.js
@@ -1,22 +1,10 @@
 const API_KEY = process.env.API_KEY
 const URL_API = process.env.URL
 URL_SYMBOL_SEARCH = process.env.URL_SYMBOL_SEARCH
-const axios = require("axios");
 const finnhub = require('finnhub');
 
 const alphaVantageApiController = { 
     tickerSearch : async (req,res) => {
-        // const symbol = req.params.caractertosearch
-        // try {    
-        // const response = await axios.get(`${URL_SYMBOL_SEARCH}${symbol}&apikey=${API_KEY}`)    
-        // const tickerSearchResults = response.data.bestMatches
-        // res.json(tickerSearchResults)
-        
-        // } catch (error) {
-        //     console.error(error);
-        //     res.status(500).render('error', error);
-        //   }
-
         const symbol = req.params.caractertosearch
         try {
           const api_key = finnhub.ApiClient.instance.authentications["api_key"];
@@ -57,18 +45,17 @@ const alphaVantageApiController = {
             console.error(error);
             res.status(500).render("error", error);
           }
-        // try {    
-        // const response = await axios.get(`${URL_API}${symbol}&apikey=${API_KEY}`)    
-        // const stockPrice = response.data
-        // res.json(stockPrice)
-        // } catch (error) {
-        //     console.error(error);
-        //     res.status(500).render('error', error);
-        //   }
     },
     companyLogo : async (req,res) => {
    
       const symbol = req.params.symbol
+
+      if (symbol.includes(".")) {
+        const errorMessage = "Symbols containing dots are not supported.";
+        console.error(errorMessage);
+        res.status(400).json({ error: errorMessage });
+        return;
+      }
       try {
         const api_key = finnhub.ApiClient.instance.authentications["api_key"];
         api_key.apiKey = "cgc550hr01qsquh3egv0cgc550hr01qsquh3egvg";
